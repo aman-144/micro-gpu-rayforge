868de2674f15ee56a989dec7a76533a22d2e7836
aman <codingyakuza@gmail.com>
2026-05-31 07:55:17 +0530

    feat: add camera ray generation per pixel

--- render.py
+++ render.py
@@ -8,9 +8,16 @@ sphere={
   "radius" : 1
 }
 image  = numpy.zeros((height,width,3),dtype=numpy.uint8)
+
+for i in range(height):
+    for j in range(width):
+        ray_origin = (0,0,0)
+        ray_direction=(j/width -0.5,i/height -0.5,-1)
+
 to_save = Image.fromarray(image)
 to_save.save("output.png")
 print("Image Saved!!")
+
 
 
 
