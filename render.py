cbae6f9a7b2a88c7a969fed3d294e62aabdf37e6
aman <codingyakuza@gmail.com>
2026-05-31 07:29:28 +0530

    feat: add sphere defination to scene

--- render.py
+++ render.py
@@ -2,7 +2,15 @@ import numpy
 from PIL import Image
 height= 600
 width =800
+
+sphere={
+  "center" : (0,0,-5),
+  "radius" : 1
+}
 image  = numpy.zeros((height,width,3),dtype=numpy.uint8)
 to_save = Image.fromarray(image)
 to_save.save("output.png")
 print("Image Saved!!")
+
+
+
